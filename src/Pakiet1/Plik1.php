4d206870ecb6b8ce21c256b90eea07c6a1a453f6
pszkudlarek1 <59268521+pszkudlarek1@users.noreply.github.com>
2019-12-27 01:32:05 +0100

    Add files via upload
    Dodane pliki

---
+++ src/Pakiet1/Plik1.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pakiet1;
+class Plik1
+{
+    public static function world()
+    {
+        return 'halo to ja';
+    }
+}
---
+++ tests/test.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+use Pakiet1\Plik1;
+echo Plik1::world();
